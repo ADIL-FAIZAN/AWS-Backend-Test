e70ecdc931c15f7d9a303dd2bfadae887a9918b6
Adil Faizan <adilrana409@gmail.com>
2026-06-16 16:54:22 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express;
+
+
+app.get('/test', (req,res) => {
+    
+ res.send('Hello, World!');
+
+
+});
+
+
+app.listen(3000, () => {
+    
+console.log('Server Running on port:3000');
+
+})
